914f9a39ee3349785c5df9e654e633bbc9e51c56
Allan Chica <ninjalover249@gmail.com>
2023-11-16 09:08:03 -0300

    Auth

--- app/(main)/(routes)/page.tsx
+++ app/(main)/(routes)/page.tsx
@@ -1,7 +1,12 @@
+import { ModeToggle } from "@/components/mode-toggle";
+import { UserButton } from "@clerk/nextjs";
+
+
 export default function Home() {
-  return (
+	return (
 		<>
-    	This is a protected route.
+			<UserButton afterSignOutUrl="/" />
+			<ModeToggle />
 		</>
-  )
+	)
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,22 +1,35 @@
+import './globals.css'
 import type { Metadata } from 'next'
 import { Open_Sans } from 'next/font/google'
-import './globals.css'
+import { ClerkProvider } from '@clerk/nextjs'
+import { ThemeProvider } from '@/components/providers/theme-provider'
+import { cn } from '@/lib/utils'
 
 const font = Open_Sans({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: 'Discord Clone',
-  description: 'Yipee',
+	title: 'Discord Clone',
+	description: 'Yipee',
 }
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
-  return (
-    <html lang="en">
-      <body className={font.className}>{children}</body>
-    </html>
-  )
+export default function RootLayout({ children, }: {	children: React.ReactNode }) {
+	return (
+		<ClerkProvider>
+			<html lang="en" suppressHydrationWarning>
+				<body className={cn(
+					font.className,
+					"bg-white dark:bg-[#313338]"
+				)}>
+					<ThemeProvider
+						attribute='class'
+						defaultTheme='dark'
+						enableSystem={false}
+						storageKey='discord-theme'
+					>
+						{children}
+					</ThemeProvider>
+				</body>
+			</html>
+		</ClerkProvider>
+	)
 }
